da8d0191af342bffbe272c5f2df39fd4d12f9f15
apvalkov <a.valkov@inxy.com>
2022-12-15 15:28:27 +0300

    v1.x: add currency resource

---
+++ tests/Webhooks/Factories/Dto/CurrenciesFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace INXY\Payments\Merchant\Tests\Webhooks\Factories\Dto;
+
+use INXY\Payments\Merchant\Tests\FactoryTest;
+use INXY\Payments\Merchant\Webhooks\Enum\ObjectName;
+use INXY\Payments\Merchant\Webhooks\Factories\Dto\CurrenciesFactory;
+
+class CurrenciesFactoryTest extends FactoryTest
+{
+    /**
+     * @return void
+     */
+    public function testCurrencyCreate()
+    {
+        $customer = CurrenciesFactory::create($this->payload);
+
+        $this->assertSame('currency_OZxelV2Bg5EoAQ0', $customer->id);
+        $this->assertSame(ObjectName::Currency, $customer->object);
+        $this->assertSame('USDT', $customer->code);
+        $this->assertSame('ethereum', $customer->blockchain);
+        $this->assertSame('erc20', $customer->coinType);
+    }
+
+    /**
+     * @return string
+     */
+    protected function payloadFilePath()
+    {
+        return 'tests/data/entities/currency.json';
+    }
+}
