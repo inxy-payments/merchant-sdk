b1ce51b1d191f17f2ef2af18780717d290a9547b
apvalkov <a.valkov@inxy.com>
2023-03-16 10:36:09 +0300

    v1.x: change sandbox url

--- src/Enums/ApiUrl.php
+++ src/Enums/ApiUrl.php
@@ -5,5 +5,5 @@ namespace INXY\Payments\Merchant\Enums;
 class ApiUrl
 {
     const Production = 'https://api.inxy.io/';
-    const Sandbox    = 'https://sandbox.api.inxy.io/';
+    const Sandbox    = 'https://sandbox-api.inxy.io/';
 }
