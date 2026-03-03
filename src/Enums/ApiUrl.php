50b0a1471aa5e8be4a164325d337a6da8bbd6d56
Anton <kroshilin.a@yandex.ru>
2026-03-03 15:29:06 +0200

    Update Sandbox URL in ApiUrl class

--- src/Enums/ApiUrl.php
+++ src/Enums/ApiUrl.php
@@ -5,5 +5,5 @@ namespace INXY\Payments\Merchant\Enums;
 class ApiUrl
 {
     public const Production = 'https://api.inxy.io/';
-    public const Sandbox    = 'https://app.sandbox-inxy.io/';
+    public const Sandbox    = 'https://api.sandbox-inxy.io/';
 }
